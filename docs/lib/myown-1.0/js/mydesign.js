48e8258ca7d611bc718013150047db7a6b20dec6
Tim <timy0616@gmail.com>
2021-04-08 16:08:45 +0800

    update

--- docs/lib/myown-1.0/js/mydesign.js
+++ docs/lib/myown-1.0/js/mydesign.js
@@ -1,4 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var el_tabs = document.querySelector(".tabs");
     var instance_tabs = M.Tabs.init(el_tabs);
+    var elem_nav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elem_nav);
+
 });
